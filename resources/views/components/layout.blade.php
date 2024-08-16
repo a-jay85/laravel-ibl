6980c4865d97a8853eeaa19f0aff79f61b3f2074
A-Jay Nicolas <ajaynicolas@gmail.com>
2024-08-15 22:36:09 -0700

    Set default values for $title

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }}</title>
+    <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
@@ -124,7 +124,7 @@
   
     <header class="bg-white shadow">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $title }}</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $title ?? config('app.name', 'Laravel') }}</h1>
       </div>
     </header>
     <main>
